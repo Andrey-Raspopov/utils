4130bde00b17a2de63d8264ee8306f71b2eba9d1
harmitage <anthonyharmitage@pm.me>
2022-12-16 15:00:49 +0400

    Linted

--- deduplicate.py
+++ deduplicate.py
@@ -1,15 +1,14 @@
+"""
+Remove all duplicate lines in the file
+"""
 import shutil
 import sys
 
-fn = sys.argv[1]
-shutil.copyfile(fn,f'{fn}.bak')
-ifile = open(f'{fn}.bak')
-ofile = open(fn, 'w')
-
-
-data = ifile.read().split('\n')
-ifile.close()
-data = list(set(data))
-data.sort()
-ofile.write('\n'.join(data))
-ofile.close()
+FILENAME = sys.argv[1]
+shutil.copyfile(FILENAME, f"{FILENAME}.bak")
+with open(f"{FILENAME}.bak", encoding="utf-8") as ifile:
+    with open(FILENAME, "w", encoding="utf-8") as ofile:
+        data = ifile.read().split("\n")
+        data = list(set(data))
+        data.sort()
+        ofile.write("\n".join(data))
